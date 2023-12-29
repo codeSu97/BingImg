70cff7fdc570e9bc1776be1e7c99422a55f2f062
codeSu <xyp_selune@163.com>
2023-12-29 23:20:03 +0800

    🐎 ci: init workflow

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from numpy import save
+
+BASE_URL = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN"
+
+IMG_PATH = "img/bing.jpg"
+
+
+def save_img(url, img_path):
+    img = requests.get(url)
+    with open(img_path, "wb") as f:
+        f.write(img.content)
+
+
+def main():
+    url_resp = requests.get(BASE_URL).json()
+    url = url_resp["images"][0]["urlbase"]
+    save_img("https://www.bing.com" + url + "_UHD.jpg", IMG_PATH)
+
+
+if __name__ == "__main__":
+    main()
